8902c5e631a2fd27fe5a79da00300b0f4ea40ff4
Jayesh Kawli <j.kawli@gmail.com>
2014-08-03 19:20:12 -0400

    successfully created model from training data, Naive bayes working on training dataset. Still need to work on probability values for newly encountered words

--- MainSpamFiltering.py
+++ MainSpamFiltering.py
@@ -4,8 +4,15 @@ from collections import Counter
 import operator
 import re
 
-def getAndFilterMessagesInDataStructureWithFileName(inputFileName):
+
+
+def getAndFilterMessagesInDataStructureWithFileName(inputFileName,frequencyOfWordsInRegularMessages,frequencyOfWordsInSpamMessages,collectionOfVectorsOfAllStatements):
+
     separatedMessageData= convertAndGetFilesDataInListFromFileWithName(inputFileName);
+
+    numberOfActualMessages=len(separatedMessageData[0]);
+    numberOfSpamMessages=len(separatedMessageData[1]);
+    #print(numberOfActualMessages," Initial ",numberOfSpamMessages);
     dynamicAttributrMappingDictionary={};
     dynamicAttributrMappingDictionary['numberOfCapitalLettersInMessage']=1;
     dynamicAttributrMappingDictionary['lengthOfLongestAllCapitalword']=2;
@@ -19,11 +26,11 @@ def getAndFilterMessagesInDataStructureWithFileName(inputFileName):
     #print(len(fullMessageDetailsWithRegularAndSpamMessage));
     filteredMessagesWithPunctuationElimination=[];
 
-    collectionOfVectorsOfAllStatements=[];
+    
     #Dictionaries which store frequency of each word occurring in each actual message and spam respectively
     #This will later be used for Naive Bayes inference
-    frequencyOfWordsInRegularMessages={};
-    frequencyOfWordsInSpamMessages={};
+    #frequencyOfWordsInRegularMessages={};
+    #frequencyOfWordsInSpamMessages={};
     
     for i,individualMessage in enumerate(fullMessageDetailsWithRegularAndSpamMessage):
         filteredMessagesWithPunctuationElimination.append(getStringWithPunctuationsRemoved(individualMessage));
@@ -33,13 +40,17 @@ def getAndFilterMessagesInDataStructureWithFileName(inputFileName):
         tokenizedDataIntoArray=list(filter(None, re.split('\W+',individualPuncutationRemovedMessage)));
         #when message is full of punctuations like e.g. :-) we get None after removing them which makes resulting string totally unusable
 
-        #if not tokenizedDataIntoArray:
-        #    continue;
+        if not tokenizedDataIntoArray:
+            if(externalIndex<len(separatedMessageData[0])):
+                numberOfActualMessages=numberOfActualMessages-1;
+            else:
+                numberOfSpamMessages=numberOfSpamMessages-1;
+            continue;
 
         vectorForGivenMessage=[-1]*len(dynamicAttributrMappingDictionary);
         
         #initialize these variable for each message that will be encountered
-        
+        #print(externalIndex);
         lengthOfLongestAllCapitalword=-1;
 
         for index,individualAttributeTokens in enumerate(tokenizedDataIntoArray):
@@ -47,8 +58,8 @@ def getAndFilterMessagesInDataStructureWithFileName(inputFileName):
 
             lengthOfCurrentSelectedToken=len(individualAttributeTokens);
             
-            #if(lengthOfCurrentSelectedToken<3):
-            #    continue;
+            if(lengthOfCurrentSelectedToken<3):
+                continue;
             if(individualAttributeTokens.isupper() and lengthOfCurrentSelectedToken>lengthOfLongestAllCapitalword):
                 vectorForGivenMessage[1]=len(individualAttributeTokens);
                 lengthOfLongestAllCapitalword=lengthOfCurrentSelectedToken;
@@ -75,21 +86,24 @@ def getAndFilterMessagesInDataStructureWithFileName(inputFileName):
          #Here end tokenizer loop and we go get next message - Might it be a spam or not   
         vectorForGivenMessage[dynamicAttributrMappingDictionary['numberOfCapitalLettersInMessage']-1]=getNumberOfCapitalLettersFromWord(individualPuncutationRemovedMessage);
         #print(tokenizedDataIntoArray," sequence tokens ");
-        if tokenizedDataIntoArray:
-            vectorForGivenMessage[dynamicAttributrMappingDictionary['lengthOfLongestWordInString']-1]=len(max(tokenizedDataIntoArray, key=len));
-            collectionOfVectorsOfAllStatements.append(vectorForGivenMessage);
+        #if tokenizedDataIntoArray:
+        vectorForGivenMessage[dynamicAttributrMappingDictionary['lengthOfLongestWordInString']-1]=len(max(tokenizedDataIntoArray, key=len));
+        collectionOfVectorsOfAllStatements.append(vectorForGivenMessage);
         #print(len(dynamicAttributrMappingDictionary));
         #Commenting for time being to eliminate garbage on cosole output screen
         #print(externalIndex," th vector is ",vectorForGivenMessage," for message "+individualPuncutationRemovedMessage);
-
+    #Last two value for 'collectionOfVectorsOfAllStatements' we append number of valid messages and spams respectively
+    #print(numberOfActualMessages," Final ",numberOfSpamMessages)
+    collectionOfVectorsOfAllStatements.append(numberOfActualMessages);
+    collectionOfVectorsOfAllStatements.append(numberOfSpamMessages);
     #We got out of even the most extrenal loop possible 
-
+    #print(len(frequencyOfWordsInRegularMessages)," fucking python ");
     #topNMostOccurringWordsInMessages=dict(Counter(frequencyOfWordsInRegularMessages).most_common(5));
     #topNMostOccurringWordsInSpams=dict(Counter(frequencyOfWordsInSpamMessages).most_common(5));
     #return frequencyOfWordsInRegularMessages,frequencyOfWordsInSpamMessages;
     #print("maessage most occurring n words ",frequencyOfWordsInRegularMessages);
     #print("spam most occurring n words ",frequencyOfWordsInSpamMessages);
-    return frequencyOfWordsInRegularMessages,frequencyOfWordsInSpamMessages;
+    #return frequencyOfWordsInRegularMessages,frequencyOfWordsInSpamMessages;
     
 
 #Warning - To use in future to counteract against differences in vector lengths
@@ -102,7 +116,71 @@ def storeInProbabilityStoreForValue(probabilityHolder,wordToUpdateProbabilityFor
     else:
         probabilityHolder[wordToUpdateProbabilityFor]=1;
                 
-frequencyOfAllWordsInGivenMessagesCorpse =[];
-frequencyOfAllWordsInGivenMessagesCorpse=getAndFilterMessagesInDataStructureWithFileName('SMSSpamCollection');
-print(frequencyOfAllWordsInGivenMessagesCorpse);
+
+def setProbabilityForOccurrenceOfEachWordInStore(inputStoreWithCountOfEachWord):
+    totalFrequencyOfAllWordsInStore=0;
+    collectionOfAllWordsInStore=inputStoreWithCountOfEachWord.keys();
+
+    for individualKey in collectionOfAllWordsInStore:
+        totalFrequencyOfAllWordsInStore=totalFrequencyOfAllWordsInStore+inputStoreWithCountOfEachWord[individualKey];
+
+    
+    for word in collectionOfAllWordsInStore:
+        inputStoreWithCountOfEachWord[word]=inputStoreWithCountOfEachWord[word]/totalFrequencyOfAllWordsInStore;
+       
+def isMessageSpam(receivedMessage,frequencyOfWordsInRegularMessages,frequencyOfWordsInSpamMessages,probabilityOfRegularMessage,probabilityOfSpamMessage):
+    tokenizedInputString = re.split('\W+',getStringWithPunctuationsRemoved(receivedMessage));
+
+    #Initialize probability values first
+    temporaryProbabilityThatMessageIsSpam=1.0;
+    temporaryProbabilityThatMessageIsNotSpam=1.0;
+    #print(frequencyOfWordsInRegularMessages,"  ",frequencyOfWordsInSpamMessages);
+    #Now using Actual Bayes Theorm;
+
+    #This is shady - They say use zero or any marginal value using minimum value among them all
+    lowestProbabilityValue=(min(min(frequencyOfWordsInRegularMessages.values()),min(frequencyOfWordsInSpamMessages.values())))/10;
+    
+
+    print("Lowest ",lowestProbabilityValue);
+    
+    for individualTokenInInputMessage in tokenizedInputString:
+        if not(individualTokenInInputMessage in frequencyOfWordsInRegularMessages):
+            temporaryProbabilityThatMessageIsNotSpam=temporaryProbabilityThatMessageIsNotSpam*lowestProbabilityValue;
+        else:
+            temporaryProbabilityThatMessageIsNotSpam=temporaryProbabilityThatMessageIsNotSpam*frequencyOfWordsInRegularMessages[individualTokenInInputMessage];
+            
+        if not(individualTokenInInputMessage in frequencyOfWordsInSpamMessages):
+            temporaryProbabilityThatMessageIsSpam=temporaryProbabilityThatMessageIsSpam*lowestProbabilityValue;
+        else:
+            temporaryProbabilityThatMessageIsSpam=temporaryProbabilityThatMessageIsSpam*frequencyOfWordsInSpamMessages[individualTokenInInputMessage];     
+
+    finalProbabilityThatMessageIsNotSpam=temporaryProbabilityThatMessageIsNotSpam*probabilityOfRegularMessage;
+    finalProbabilityThatMessageIsSpam=temporaryProbabilityThatMessageIsSpam*probabilityOfSpamMessage;
+    print(finalProbabilityThatMessageIsNotSpam," and ",finalProbabilityThatMessageIsSpam);
+
+frequencyOfWordsInRegularMessages={};
+frequencyOfWordsInSpamMessages={};
+collectionOfVectorsOfAllStatements=[];
+
+getAndFilterMessagesInDataStructureWithFileName('SMSSpamCollection',frequencyOfWordsInRegularMessages,frequencyOfWordsInSpamMessages,collectionOfVectorsOfAllStatements);
+
+#Last two value of collectionOfVectorsOfAllStatements contains number of actual messages and spams respectively
+#P.S. Last value is number of spam messages out of total received message corpse
+
+
+setProbabilityForOccurrenceOfEachWordInStore(frequencyOfWordsInRegularMessages);
+setProbabilityForOccurrenceOfEachWordInStore(frequencyOfWordsInSpamMessages);
+
+#Second last value
+totalNumberRegularMessagesAfterFiltering=collectionOfVectorsOfAllStatements[-2];
+#Last value
+totalNumberSpamMessagesAfterFiltering=collectionOfVectorsOfAllStatements[-1];
+
+
+
+probabilityOfRegularMessage=(totalNumberRegularMessagesAfterFiltering/(totalNumberRegularMessagesAfterFiltering+totalNumberSpamMessagesAfterFiltering));
+probabilityOfSpamMessage=(totalNumberSpamMessagesAfterFiltering/(totalNumberRegularMessagesAfterFiltering+totalNumberSpamMessagesAfterFiltering));
+#print(probabilityOfRegularMessage," **************************  ",probabilityOfSpamMessage);
+
+print(isMessageSpam("Oh you got many responsibilities.",frequencyOfWordsInRegularMessages,frequencyOfWordsInSpamMessages,probabilityOfRegularMessage,probabilityOfSpamMessage));
 
